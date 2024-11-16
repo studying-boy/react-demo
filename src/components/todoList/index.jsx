35c9f0f721c3d73fea51b3900348a73a17237768
liquansheng@wisevar_hm.onaliyun.com <liquansheng@wisevar_hm.onaliyun.com>
2024-11-16 21:44:43 +0800

    todoList增加过滤功能

--- src/components/todoList/index.jsx
+++ src/components/todoList/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Checkbox, Button } from 'antd';
 import './index.scss';
 import { useNavigate } from 'react-router-dom';
@@ -7,11 +7,19 @@ import { updateData, removeData } from '../../store/todoSlice';
 
 export default function TodoList(props) {
   const { todoList } = props;
+  const [ todoListShow, updateTodoListShow ] = useState(todoList);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    console.log(isFilterFinally);
+    updateTodoListShow(isFilterFinally ? todoList.filter(item => !item.checked) : todoList);
+  }, [todoList])
+
+  const [isFilterFinally, setIsFilterFinally] = useState(false);
+
   const changeCheckbox = (index) => {
-    dispatch(updateData(index))
+    dispatch(updateData(index));
   }
 
   const deleteItem = (index) => {
@@ -22,16 +30,25 @@ export default function TodoList(props) {
     navigate(`/about?num=${index}`);
   }
 
+  const changeFilter = () => {
+    const currentRealResult = !isFilterFinally; // 当前真正的值
+    setIsFilterFinally(currentRealResult);
+    updateTodoListShow(currentRealResult ? todoList.filter(item => !item.checked) : todoList);
+  }
+
   return (
     <div className='todoList-container'>
-      {todoList.map((data, index) => {
-        return <div key={data.text + index} className='item'>
-          <Checkbox checked={data.checked} onChange={() => changeCheckbox(index)}></Checkbox>
+      {todoListShow.map((data) => {
+        return <div key={data.id} className='item'>
+          <Checkbox checked={data.checked} onChange={() => changeCheckbox(data.id)}></Checkbox>
           <span className={data.checked ? 'text isChecked' : 'text'}>{data.text}</span>
-          <Button type="link" onClick={() => editItem(index)}>编辑</Button>
-          <Button type="link" onClick={() => deleteItem(index)}>删除</Button>
+          <Button type="link" onClick={() => editItem(data.id)}>编辑</Button>
+          <Button type="link" onClick={() => deleteItem(data.id)}>删除</Button>
         </div>
       })}
+      <div style={{ marginTop: '20px' }}>
+        <Checkbox checked={isFilterFinally} onChange={changeFilter}></Checkbox> 过滤已完成项
+      </div>
     </div>
   )
 }
--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -2,9 +2,11 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   list: [{
+    id: 0,
     checked: false,
     text: '第一条写死的数据'
   }, {
+    id: 1,
     checked: true,
     text: '第二条写死的数据'
   }]
@@ -16,16 +18,18 @@ const todoSlice = createSlice({
   reducers: {
     addData: (state, action) => {
       state.list.push({
+        id: state.list.length,
         checked: false,
         text: action.payload
       });
     },
     removeData: (state, action) => {
-      state.list.splice(action.payload, 1);
+      const index = state.list.findIndex((item => item.id == action.payload));
+      state.list.splice(index, 1);
     },
     updateData: (state, action) => {
-      console.log(action);
-      state.list[action.payload].checked = !state.list[action.payload].checked;
+      const index = state.list.findIndex((item => item.id == action.payload))
+      state.list[index].checked = !state.list[index].checked;
     },
   }
 })
